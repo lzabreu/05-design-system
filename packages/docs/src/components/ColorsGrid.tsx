ce9842d3dc007cd7a06b6025f5107e19391e4a39
Luciano Abreu <lzabreu@mac.com>
2023-07-12 19:50:26 -0300

    first commit

---
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -0,0 +1,15 @@
+import {colors} from '@ignite-ui/tokens'
+import {getContrast} from 'polished'
+
+export function ColorsGrid(){
+  return Object.entries(colors).map(([key, color]) =>{
+    return ( 
+    <div style={{backgroundColor: color, padding: '1rem'}} key={key}>
+      <div style={{display: 'flex', justifyContent: 'space-between', fontFamily: 'monospace', color: getContrast(color, '#fff') < 3.5 ? 'black' : 'white'}}>
+        <strong>${key}</strong>
+        <span>{color}</span>
+      </div>
+    </div>
+  )
+  })
+}
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,29 @@
+import { ComponentProps } from 'react'
+import { styled } from './styles'
+
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+  borderRadius: '$sm',
+  border: 0,
+  fontWeight: '$bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: '$xs',
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: '$md',
+        padding: '$3 $6',
+      },
+    },
+  },
+  defaultVariants: {
+    size: 'small',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button>
